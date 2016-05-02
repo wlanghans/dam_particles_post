dd5b37efe647e6a4244e7a1f9516ac87915b4422
Wolfgang Langhans <wlanghans@lbl.gov>
2016-05-02 16:12:18 -0700

    adapted things

--- src_dry/module_particle.F90
+++ src_dry/module_particle.F90
@@ -1,11 +1,11 @@
 module particle_data
   implicit none
 
-  integer, parameter :: N_Scalar = 9
+  integer, parameter :: N_Scalar = 15
 
   real(8), parameter :: qc_cutoff = 1.d-5, w_cutoff = -100.d0
 
-  character(*), dimension(N_Scalar), parameter :: var_name =(/"rho","qa","qv","qc","qr","qi","t","tabs","p"/)
+  character(*), dimension(N_Scalar), parameter :: var_name =(/"rho","qa","qv","qc","qr","qi","t","tabs","p","b","Fdyn","Fbuoy","Fbuoyd","Fbuoyv","Fbuoyc"/)
   character(*), dimension(3), parameter :: Coord_var_name =(/"x","y","z"/)
   character(*), dimension(3), parameter ::   Vel_var_name =(/"u","v","w"/)
 
